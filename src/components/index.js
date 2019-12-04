c29ebe9315eacd049eec8d54da78d1dd128cf263
zaruba2004 <zaruba2004@gmail.com>
2019-12-04 04:56:44 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,40 @@
+import React, {Component, Fragment} from "react";
+import Header from '../includes/header/header'
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import HomePage from "./../pages/main/index";
+import CatalogPage from "./../pages/catalog/index";
+import NewsPage from "./../pages/news/index";
+import NotFound from "./../pages/404/index";
+import NewsItemPage from "./../pages/news/itemInfo";
+import ContactsPage from "./../pages/contacts/index";
+
+export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items : [
+
+            ]
+
+    }
+
+    // f3b3675c-c7bf-453e-ab4b-c70537a72ccd
+
+    render() {
+        return (
+            <Fragment>
+                <BrowserRouter>
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/" component={HomePage}/>
+                        <Route exact path="/catalog" component={CatalogPagen items={this.state.items}}/>
+                        <Route exact path="/news" component={NewsPage}/>
+                        <Route exact path="/news/post/:id" component={NewsItemPage}/>
+                        <Route exact path="/contacts" component={ContactsPage}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </BrowserRouter>
+            </Fragment>
+        )
+    }
+}
---
+++ src/reduces/index.js
@@ -0,0 +1,9 @@
+import comments from "./comment";
+import items from "./items";
+
+import {combineReducers} from "redux";
+const reduces = combineReducers({
+    comments,items
+})
+
+export default reduces;
